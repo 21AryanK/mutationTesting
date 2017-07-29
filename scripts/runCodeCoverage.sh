afb8845324db6c4ea1c26c35c859efea806ac6ea
John Farrelly <johnnyleitrim@gmail.com>
2017-07-29 21:52:22 +0100

    Correct error in report argument

--- scripts/runCodeCoverage.sh
+++ scripts/runCodeCoverage.sh
@@ -3,5 +3,5 @@
 SCRIPT_DIR=$(dirname $0)
 source $SCRIPT_DIR/procs.sh
 
-py.test --cov-report html --cov=simplenumber tests/
+pytest --cov-report=html --cov=simplenumber tests/
 openUrl htmlcov/index.html
